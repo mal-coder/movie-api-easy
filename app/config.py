4f452536f6b238bc4fbc56b7ee9a6c68844c2b3c
MAL <maciej.laskowski@applifting.cz>
2021-04-18 01:32:29 +0200

    changes to required settings and error handling

--- app/config.py
+++ app/config.py
@@ -1,10 +1,12 @@
 import json
 from os import environ
 
-painless = False
 xml_api_uri = environ['XML_API_URI']
 api_key = environ['API_KEY']
-xml_attributes = json.loads(environ['XML_ATTRIBUTES'])
-port = environ['PORT']
-host = environ['HOST']
+xml_query_parameter = environ.get('XML_QUERY_PARAMETER', 't')
+xml_attributes = json.loads(environ.get('XML_ATTRIBUTES',
+                                        '["title", "year", "rated", "released", "runtime", "genre", "director", "writer", "actors", "plot", "language", "country", "awards", "poster", "metascore", "imdbRating", "imdbVotes", "imdbID", "type"]'))
+port = environ.get('PORT', '5000')
+host = environ.get('HOST', '0.0.0.0')
 parameter = environ.get('QUERY_PARAMETER', 'title')
+painless = False
--- app/resources/movie.py
+++ app/resources/movie.py
@@ -3,8 +3,9 @@ from xml.etree import ElementTree
 
 import requests
 from flask_restful import Resource, abort
+from werkzeug.exceptions import NotFound
 
-from app.config import xml_api_uri, xml_attributes, painless, parameter
+from app.config import xml_api_uri, xml_attributes, painless, parameter, xml_query_parameter
 from app.security.authentication import authenticate_token
 from app.validation.request_parser import parser
 
@@ -14,7 +15,7 @@ class MovieEndpoint(Resource):
     def get(self):
         title = parser.parse_args().get(parameter, None)
         try:
-            url = f'{xml_api_uri}&t={title}'
+            url = f'{xml_api_uri}&{xml_query_parameter}={title}'
             response = requests.get(url)
             response.raise_for_status()
 
@@ -27,6 +28,8 @@ class MovieEndpoint(Resource):
                         return child.attrib
             else:
                 content = response.text
+                if '<error>Movie not found!</error>' in content:
+                    abort(404, message='Movie not found!')
                 movie_data = dict()
                 for attr in xml_attributes:
                     attr_lindex = content.find(attr)
@@ -41,6 +44,8 @@ class MovieEndpoint(Resource):
             if response.text:
                 message = response.text
             abort(500, message=message)
+        except NotFound:
+            raise
         except Exception:
             logging.exception("Error while processing request")
             abort(500, message=f'Unknown server error')
